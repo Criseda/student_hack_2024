8d911e6f1eeb92ff6a76c5265ae9c9dfcdf3ad65
criseda <laurentiu.cristian.preda@gmail.com>
2024-04-13 21:37:10 +0100

    Added useEffect to scroll back to the top on reload of page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,20 @@
+"use client";
+import { useEffect } from "react";
 import HomePage from "@/components/HomePage";
 import Navbar from "@/components/Navbar";
 import Planets from "@/components/Planets";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <div className="relative w-full">
         <Navbar />
         <HomePage />
       </div>
-      <div className="h-auto w-screen flex justify-center items-center">
-        <Planets />
-      </div>
+      <Planets />
     </>
   );
 }
