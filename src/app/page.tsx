da6792a1c3f866dd35909a3529311167fb6e6710
a38062an <aduyanhn@gmail.com>
2024-04-13 16:03:21 +0100

    added in loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import { HomePage } from "@/components/HomePage";
+import { Suspense } from "react";
+import Loading  from "./loading";
+
 
 export default function Home() {
   return (
+  <Suspense fallback={<Loading />}>
     <>
       <HomePage />
     </>
+  </Suspense>
   );
+
 }
