55066cafabf1284a4eec471524ed82dee9d2504d
criseda <laurentiu.cristian.preda@gmail.com>
2024-04-13 18:57:54 +0100

    Added the planets to /home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,17 @@
-import { HomePage } from "@/components/HomePage";
+import HomePage from "@/components/HomePage";
 import Navbar from "@/components/Navbar";
-
-
+import Planets from "@/components/Planets";
 
 export default function Home() {
   return (
-    <div className="relative w-full">
-      <Navbar />
-      <HomePage />
-      <p>filler to create scroll on the right</p>
-    </div>
+    <>
+      <div className="relative w-full">
+        <Navbar />
+        <HomePage />
+      </div>
+      <div className="h-auto w-screen flex justify-center items-center">
+        <Planets />
+      </div>
+    </>
   );
 }
