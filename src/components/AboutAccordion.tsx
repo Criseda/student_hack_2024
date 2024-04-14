130f3461026e6a59f98f7f252c3ebf41b1d8ad8d
criseda <laurentiu.cristian.preda@gmail.com>
2024-04-14 10:38:41 +0100

    Changesd Accordion with new data

--- src/components/AboutAccordion.tsx
+++ src/components/AboutAccordion.tsx
@@ -13,19 +13,13 @@ export function AboutAccordion() {
       <div className="max-w-2xl mx-auto p-4 mt-4">
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
-            <AccordionTrigger className="text-lg font-bold">How does the website implement accesibility?</AccordionTrigger>
-            <AccordionContent>
-              We have implemented colour blind modes in order to make the website more accessible to everyone.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
             <AccordionTrigger className="text-lg font-bold">Who is this website for?</AccordionTrigger>
             <AccordionContent>
               Anyone! If you are interested in learning about space then come along for the ride! 
               components&apos; aesthetic.
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-3">
+          <AccordionItem value="item-2">
             <AccordionTrigger className="text-lg font-bold">How do I use it?</AccordionTrigger>
             <AccordionContent>
               Click on a Planet to learn about it and then once your done rading do a quiz to test yourself!
@@ -34,7 +28,14 @@ export function AboutAccordion() {
           <AccordionItem value="item-3">
             <AccordionTrigger className="text-lg font-bold">Who made the website?</AccordionTrigger>
             <AccordionContent>
-              Chris, Romanian Chris, Anthony and Alex made the website! 
+              Chris, Romanian Cris, Anthony and Alex made the website! 
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger className="text-lg font-bold">What on Earth are these quizzes??</AccordionTrigger>
+            <AccordionContent>
+              These quizzes are supposed to be fun for children, hence some of the questions are a bit silly.
+              Throughout those silly questions there also are some more educational ones to develop the children&apos;s knowledge.
             </AccordionContent>
           </AccordionItem>
 
