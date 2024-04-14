de2f12465e3716b4baeb5165db2eed13ad40462d
criseda <laurentiu.cristian.preda@gmail.com>
2024-04-14 04:49:20 +0100

    Implemented SolarMenu within page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import HomePage from "@/components/HomePage";
 import Navbar from "@/components/Navbar";
 import Planets from "@/components/Planets";
+import SolarMenu from "@/components/SolarMenu";
 
 export default function Home() {
   useEffect(() => {
@@ -11,9 +12,12 @@ export default function Home() {
   return (
     <>
       <div className="relative w-screen overflow-hidden">
-          <Navbar />
-          <HomePage />
-          <Planets />
+        <Navbar />
+        <HomePage />
+        <Planets />
+        <div className="fixed top-5 right-10 z-50">
+          <SolarMenu />
+        </div>
       </div>
     </>
   );
